d8f9c9274bf3caf969f117e9c773af97414c8029
Amith Shivantha <kdashivantha@gmail.com>
2019-12-27 15:37:17 +0530

    basic app structure added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MindMapComponent } from './mind-map/mind-map.component';
+import { NodeDetailComponent } from './node-detail/node-detail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MindMapComponent,
+    NodeDetailComponent
   ],
   imports: [
     BrowserModule
