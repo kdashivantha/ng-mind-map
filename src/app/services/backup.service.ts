9d0cc7a313a179f854993a634c32f50185cc6ff1
Amith Shivantha <kdashivantha@gmail.com>
2019-12-27 16:04:03 +0530

    add local storage

--- src/app/services/backup.service.ts
+++ src/app/services/backup.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { LocalStorage } from '@ngx-pwa/local-storage';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BackupService {
+  private _dbKey:string = "db";
 
-  constructor() { }
+  constructor(private localStorage: LocalStorage) {
+      
+  }
+
+  /**
+   * write data to Localstorage
+   * @param data 
+   */
+  public WriteToLocalStorage(data:any){
+      this.localStorage.setItem(this._dbKey, JSON.stringify(data)).subscribe(() => {});
+  }
+
+  /**
+   * read data from local storage
+   * @param key 
+   */
+  public ReadFromLocalStorage():Observable<any>{
+      return this.localStorage.getItem(this._dbKey).pipe(
+          map((data:string)=> JSON.parse(data))
+      );
+  }
 }
